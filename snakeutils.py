532438f7631f80cf430466112d5bb2b36bea948c
Felix He <36784400+felixhe97@users.noreply.github.com>
2020-06-17 17:16:47 -0700

    Add files via upload

---
+++ snakeutils.py
@@ -0,0 +1,30 @@
+EMPTY = 0
+
+FOOD = 1
+
+# you are 2, opponents are 3 - 9
+INDEXSTART = 2
+
+def printboard(board):
+    for row in board:
+        print(row)
+
+def printsnakes(snakes):
+    for i, snake in snakes.items():
+        print(i, snake)
+
+def printfood(food):
+    for x in food:
+        print(x)
+
+def moveup():
+    return {'move': 'up', 'shout': 'gangg'}
+
+def moveleft():
+    return {'move': 'left', 'shout': 'ganggang'}
+
+def moveright():
+    return {'move': 'right', 'shout': 'swerve'}
+
+def movedown():
+    return {'move': 'down', 'shout': 'skrrrt'}
