8dbd3da8beeb72b5f872c00163056522b1f6b245
felixhe97 <felixhe97@gmail.com>
2020-06-18 15:53:33 -0700

    removing unnecessary subdirectories

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,41 @@ import db
 
 import utils
 
+def createsnakeobj(snake, index, graph):
+    coordarr = []
+    for coord in snake['body']:
+        coordarr.append([coord['x'], coord['y']])
+        graph[coord['x']][coord['y']] = index
+    snakeObj = {
+            'id': snake['id'],
+            'hp': snake['health'],
+            'body': coordarr,
+            'head': [snake['head']['x'], snake['head']['y']],
+            'length': snake['length']
+            }
+    return snakeObj
+
+def initboard(mi, mj, foodarr, opponents, mysnake):
+    matrix = [[utils.EMPTY] * mi for i in range(mj)]
+    tempfood = []
+    for food in foodarr:
+        matrix[food['x']][food['y']] = utils.FOOD
+        tempfood.append([food['x'],food['y']])
+    snakemap = {}
+    index = utils.INDEXSTART
+    for snake in opponents:
+        snakemap[index] = createsnakeobj(snake, index, matrix)
+        index = index + 1
+    return {'board': matrix, 'food': tempfood, 'snakes': snakemap}
+
+def merge(prevstate, newinfo):
+    # TODO
+    return
+
+def nextmove(board, food, snakes):
+    # TODO
+    return
+
 inmem = {}
 
 def end(jsonobj):
